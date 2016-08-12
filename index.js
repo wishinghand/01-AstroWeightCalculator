1d6dd38763b85201a723c69c46382f99bf13d851
wishinghand <wishinghand@gmail.com>
2016-08-12 09:53:24 -0700

    removed crazy switch statement

--- index.js
+++ index.js
@@ -1,142 +1,94 @@
-$("#btnCalculate").click(function(){
-    var userWeight = $('#weight').val();
-    var planetText = $('#planet option:selected').text();
-    var planetGrav = $('#planet option:selected').val();
-    var weightOnPlanet = userWeight * planetGrav;
-    $('#output').text("If you were on " + planetText + " you would weigh " +weightOnPlanet + " lbs!");
-});
+'use strict';
 
+//object store of planets, easy to add more
+var planets = [
+    {
+        name: "Sun",
+        imagePath: "/images/thesun.jpg",
+        weightMultiplier: 27.9
+    },
+    {
+        name: "Mercury",
+        imagePath: "/images/Mercury.jpg",
+        weightMultiplier: 0.377
+    },
+    {
+        name: "Venus",
+        imagePath: "/images/Venus.jpg",
+        weightMultiplier: 0.9032
+    },
+    {
+        name: "Earth",
+        imagePath: "/images/earth.jpg",
+        weightMultiplier: 1
+    },
+    {
+        name: "Moon",
+        imagePath: "/images/moon.jpg",
+        weightMultiplier: 0.1655
+    },
+    {
+        name: "Mars",
+        imagePath: "/images/mars.jpg",
+        weightMultiplier: 0.3895
+    },
+    {
+        name: "Jupiter",
+        imagePath: "/images/jupiter.png",
+        weightMultiplier: 2.640
+    },
+    {
+        name: "Saturn",
+        imagePath: "/images/saturn.jpg",
+        weightMultiplier: 1.139
+    },
+    {
+        name: "Uranus",
+        imagePath: "/images/Uranus2.jpg",
+        weightMultiplier: 0.917
+    },
+    {
+        name: "Neptune",
+        imagePath: "/images/neptune.jpg",
+        weightMultiplier: 1.148
+    },
+    {
+        name: "Pluto",
+        imagePath: "/images/pluto.jpg",
+        weightMultiplier: 0.06
+    }
+];
 
-// 'use strict';
+//dynamically creating the options in the select element
+var select = document.getElementById('planet');
+var docFrag = document.createDocumentFragment();
 
-// //object store of planets, easy to add more
-// var planets = [
-//     {
-//         name: "Sun",
-//         imagePath: "/images/thesun.jpg",
-//         weightMultiplier: 27.9
-//     },
-//     {
-//         name: "Mercury",
-//         imagePath: "/images/Mercury.jpg",
-//         weightMultiplier: 0.377
-//     },
-//     {
-//         name: "Venus",
-//         imagePath: "/images/Venus.jpg",
-//         weightMultiplier: 0.9032
-//     },
-//     {
-//         name: "Earth",
-//         imagePath: "/images/earth.jpg",
-//         weightMultiplier: 1
-//     },
-//     {
-//         name: "Moon",
-//         imagePath: "/images/moon.jpg",
-//         weightMultiplier: 0.1655
-//     },
-//     {
-//         name: "Mars",
-//         imagePath: "/images/mars.jpg",
-//         weightMultiplier: 0.3895
-//     },
-//     {
-//         name: "Jupiter",
-//         imagePath: "/images/jupiter.png",
-//         weightMultiplier: 2.640
-//     },
-//     {
-//         name: "Saturn",
-//         imagePath: "/images/saturn.jpg",
-//         weightMultiplier: 1.139
-//     },
-//     {
-//         name: "Uranus",
-//         imagePath: "/images/Uranus2.jpg",
-//         weightMultiplier: 0.917
-//     },
-//     {
-//         name: "Neptune",
-//         imagePath: "/images/neptune.jpg",
-//         weightMultiplier: 1.148
-//     },
-//     {
-//         name: "Pluto",
-//         imagePath: "/images/pluto.jpg",
-//         weightMultiplier: 0.06
-//     }
-// ];
+for (var i = 0; i < planets.length; i++) {
+    var option = document.createElement('option');
+    option.value = i;
+    option.text = planets[i].name;
+    docFrag.appendChild(option);
+}
 
-// //dynamically creating the options in the select element
-// (function(){
-//     var select = document.getElementById('planet');
-//     var docFrag = document.createDocumentFragment();
+select.appendChild(docFrag);
 
-//     for (var i = 0; i < planets.length; i++) {
-//         var option = document.createElement('option');
-//         option.value = planets[i].weightMultiplier;
-//         option.text = planets[i].name;
-//         docFrag.appendChild(option);
-//     }
+//called with onclick on <button> element
+function calculate(){
+    var planet = planets[parseFloat(document.getElementById('planet').value)];
+    var humanWeight = parseFloat(document.getElementById('weight').value);
+    var output = document.getElementById('output');
+    var newWeight = humanWeight * planet.weightMultiplier;
 
-//     select.appendChild(docFrag);
-// }());
+    //error checking
+    if (humanWeight <= 0) {
+        output.innerHTML = 'I doubt you weigh that little- try a positive number';
+        return;
+    }else if(isNaN(newWeight)){
+        output.innerHTML = 'Sorry buddy, you gotta use a positive number';
+        return;
+    }
 
-// //called with onclick on <button> element
-// function calculate(){
-//     var humanWeight = parseFloat(document.getElementById('weight').value);
-//     var planetMultiplier = parseFloat(document.getElementById('planet').value);
-//     var output = document.getElementById('output');
-//     var newWeight = humanWeight * planetMultiplier;
-
-//     //error checking
-//     if (humanWeight <= 0) {
-//         output.innerHTML = 'I doubt you weigh that little- try a positive number';
-//         return;
-//     }else if(isNaN(newWeight)){
-//         output.innerHTML = 'Sorry buddy, you gotta use a positive number';
-//         return;
-//     }
-
-//     // final texual output of the calculation
-//     output.innerHTML = "Your new weight: " + newWeight;
-//     output.innerHTML += '<br><img src="' + planets[planetMultiplier.selectedIndex].imagePath + '">';
-
-//     //brute force image pairing with the object. Inelegant, but it works. But also brittle.
-//     // switch(planetMultiplier){
-//     //     case 27.9:
-//     //         output.innerHTML += '<br><img src="' + planets[0].imagePath + '">';
-//     //         break;
-//     //     case 0.377:
-//     //         output.innerHTML += '<br><img src="' + planets[1].imagePath + '">';
-//     //         break;
-//     //     case 0.9032:
-//     //         output.innerHTML += '<br><img src="' + planets[2].imagePath + '">';
-//     //         break;
-//     //     case 1:
-//     //         output.innerHTML += '<br><img src="' + planets[3].imagePath + '">';
-//     //         break;
-//     //     case 0.1655:
-//     //         output.innerHTML += '<br><img src="' + planets[4].imagePath + '">';
-//     //         break;
-//     //     case 0.3895:
-//     //         output.innerHTML += '<br><img src="' + planets[5].imagePath + '">';
-//     //         break;
-//     //     case 2.640:
-//     //         output.innerHTML += '<br><img src="' + planets[6].imagePath + '">';
-//     //         break;
-//     //     case 1.139:
-//     //         output.innerHTML += '<br><img src="' + planets[7].imagePath + '">';
-//     //         break;
-//     //     case 0.917:
-//     //         output.innerHTML += '<br><img src="' + planets[8].imagePath + '">';
-//     //         break;
-//     //     case 1.148:
-//     //         output.innerHTML += '<br><img src="' + planets[9].imagePath + '">';
-//     //         break;
-//     //     case 0.06:
-//     //         output.innerHTML += '<br><img src="' + planets[10].imagePath + '">';
-//     //         break;
-//     // }
-// }
+    // final texual output of the calculation
+    output.innerHTML = "Your new weight: " + newWeight;
+    output.innerHTML += '<br><img src="' + planet.imagePath + '">';
+}
